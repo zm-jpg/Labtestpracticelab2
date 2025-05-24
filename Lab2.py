df15136a404406502e5b3cae1a32f431e4ff123f
ongzongming <zooming.24@ichat.sp.edu.sg>
2025-05-24 22:47:03 +0800

    calculations for median

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,4 @@
+import statistics as stats
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67,32)")
 
@@ -21,13 +22,19 @@ def calc_min_max_temperature(num):
     result=[minimum,maximum]
     return result
 
+def calc_median_temperature(num):
+    median=stats.median(num)
+    return median
+
 def main():
     display_main_menu()
     num=get_user_input()
     average=calc_average_temperature(num)
     minmax=calc_min_max_temperature(num)
+    median=calc_median_temperature(num)
     print(str(average))
     print("minimum , maximum ="+str(minmax))
+    print("median = " +str(median))
 
 
 
